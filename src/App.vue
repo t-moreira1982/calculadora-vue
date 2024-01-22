87560a32f9b9dab8aa49c9c4e7e43f6e24ee08e1
Thiago <thiago_moreira22@hotmail.com>
2024-01-22 16:27:11 -0300

    Calculadora com VueJS

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,113 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive, watch } from 'vue'
+
+const estado = reactive({
+    primeiroNumero: 0,
+    segundoNumero: 0,
+    resultado: 0,
+    operacao: ''
+})
+
+const calcularResultado = () => {
+
+    switch (estado.operacao) {
+        case "soma":
+            estado.resultado = estado.primeiroNumero + estado.segundoNumero
+            break;
+        case 'subtracao':
+            estado.resultado = estado.primeiroNumero - estado.segundoNumero
+            break;
+        case 'multiplicacao':
+            estado.resultado = estado.primeiroNumero * estado.segundoNumero
+            break;
+        case 'divisao':
+            estado.resultado = estado.primeiroNumero / estado.segundoNumero
+            break;
+        default:
+            estado.resultado = 0
+            break;
+    }
+    console.log(primeiroNumero)
+    console.log(segundoNumero)
+    console.log(estado.resultado)
+    console.log(estado.operacao)
+}
+
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <div class="body">
+        <div class="container">
+            <header>
+                <h1>Calculadora com VueJS</h1>
+            </header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+            <form>
+                <div class="calc mt-4">
+                    <div class="form-group me-3">
+                        <label for="primeiroNumero">Digite o primeiro número</label>
+                        <input @input="calcularResultado" v-model="estado.primeiroNumero" type="number"
+                            class="form-control" placeholder="Primeiro Número">
+                    </div>
+                    <div class="form-group">
+                        <label for="segundoNumero">Digite o segundo número</label>
+                        <input @input="calcularResultado" v-model="estado.segundoNumero" type="number"
+                            class="form-control" placeholder="Segundo Número">
+                    </div>
+                </div>
+                <div class="form-group operacao">
+                    <label for="escolhaDaOperacao"><strong>Escolha a operação</strong></label>
+                    <select @change="calcularResultado" v-model="estado.operacao" name="escolhaDaOperacao" id="escolhaDaOperacao">
+                        <option value="soma">Soma</option>
+                        <option value="subtracao">Subtração</option>
+                        <option value="multiplicacao">Multiplicação</option>
+                        <option value="divisao">Divisão</option>
+                    </select>
+                </div>
+                <div class="resultado mt-5">
+                    <h3>O resultado da operação é <span> <strong>{{ estado.resultado }}</strong> </span></h3>
+                </div>
+            </form>
+        </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
+.body {
+    background-color: rgb(247, 245, 245);
+}
+
 header {
-  line-height: 1.5;
+    color: rgb(32, 32, 182);
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: rgb(221, 212, 212);
+    min-height: 100vh;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.calculator-form {
+    width: 100%;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
+.calc,
+.operacao {
     display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    justify-content: center;
+    padding: 20px;
+}
+
+.resultado {
+    text-align: center;
+}
+
+span {
+    color: rgb(189, 31, 31);
 }
 </style>
